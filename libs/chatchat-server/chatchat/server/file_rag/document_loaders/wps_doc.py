48934d5d5bbdda1cc9fd440a0a6aec995ef69806
zpyzl <zpyzl@qq.com>
2024-12-20 14:41:04 +0800

    add wps doc files

--- libs/chatchat-server/chatchat/server/file_rag/document_loaders/wps_doc.py
+++ libs/chatchat-server/chatchat/server/file_rag/document_loaders/wps_doc.py
@@ -1,7 +1,7 @@
 from chatchat.server.file_rag.document_loaders.file_loader_service import read_wps
 
 if __name__ == '__main__':
-    r = read_wps(r"D:\test_rag_doc\doc_wps\杭州英普环境技术股份有限公司(编号2477).wps")
+    r = read_wps(r"杭州英普环境技术股份有限公司(编号2477).wps")
     print(r)
-    r = read_wps(r"D:\test_rag_doc\doc_wps\公文智能排版与校对系统-管理员手册.doc")
+    r = read_wps(r"公文智能排版与校对系统-管理员手册.doc")
     print(r)
